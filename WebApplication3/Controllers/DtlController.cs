24c788927d30a672c43d6b64a377542ccfe135c3
Ulviyya-R <ulviyyamr@code.edu.az>
2023-02-27 01:46:06 +0400

    product detail  page created

---
+++ WebApplication3/Controllers/DtlController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication3.Controllers
+{
+	public class DtlController:Controller
+	{
+		public ActionResult Index()
+		{
+			return View();
+		}
+	}
+}
--- WebApplication3/Program.cs
+++ WebApplication3/Program.cs
@@ -2,6 +2,6 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
 var app = builder.Build();
 
-app.MapControllerRoute("default", "{controller=products}/{action=index}");
+app.MapControllerRoute("default", "{controller=home}/{action=index}");
 
 app.Run();
